50efe9d690e5ef87b075ffdd554a6a935ab9750a
Lillian Ng <lillian.k.ng@gmail.com>
2014-01-14 21:38:17 -0800

    added welcome page. Fixes #9

--- test/features/can_access_home_test.rb
+++ test/features/can_access_home_test.rb
@@ -2,8 +2,8 @@ require "test_helper"
 
 class CanAccessHomeTest < Capybara::Rails::TestCase
   test "sanity" do
-    visit root_path
-    assert_content page, "Hello World"
+    visit '/'
+    assert_content page, "Lillian Ng"
     refute_content page, "Goobye All!"
   end
 end
