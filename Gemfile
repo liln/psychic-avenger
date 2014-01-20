d92f9471ccfda66e880a37ac38420cbdf7e05223
Lillian Ng <lillian.k.ng@gmail.com>
2014-01-20 15:33:18 -0800

    changed template, updated assets

--- Gemfile
+++ Gemfile
@@ -12,7 +12,7 @@ gem 'rails', '4.0.2'
 gem 'sass-rails', '~> 4.0.0'
 gem 'turbolinks'
 gem 'uglifier', '>= 1.3.0'
-gem 'foundation-rails', github: 'codefellows/foundation-rails'
+gem 'foundation-rails', '~> 5.0.3'
 
 group :doc do
   gem 'sdoc', require: false
--- test/features/check_for_zurb_test.rb
+++ test/features/check_for_zurb_test.rb
@@ -1,13 +1,12 @@
 require "test_helper"
 
 feature "Check for Zurb" do
-  scenario "root page contains columns" do
+  scenario "foundation is installed" do
     # Given I load the root
     # When I load the root
     visit root_path
 
     # Zurb is loaded if there are columns
-    page.body.must_match 'foundation'
-    page.body.must_match 'columns'
+    assert page.has_css? ".columns"
   end
 end
