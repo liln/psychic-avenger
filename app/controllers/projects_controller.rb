aa5f29b41a54ea2c88565574e833a35b04a012a4
Lillian Ng <lillian.k.ng@gmail.com>
2014-02-11 15:16:34 -0800

    Ajaxifed projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -12,12 +12,22 @@ class ProjectsController < ApplicationController
 
   def create
     @project = Project.new(project_params)
-    if @project.save
-      flash[:notice] = "Project has been created."
-      redirect_to @project
-    else
-      flash.now[:error] = "Project could not be saved."
-      render :new
+    respond_to do |format|
+      format.html do
+        if @project.save
+          flash[:notice] = "Project has been created."
+          redirect_to @project
+        else
+          flash.now[:error] = "Project could not be saved."
+          render :new
+        end
+      end
+      format.js do
+        if @project.save
+        else
+          render text: @project.errors.full_messages.join(". "), status: :unprocessable_entity
+        end
+      end
     end
   end
 
@@ -28,17 +38,30 @@ class ProjectsController < ApplicationController
   end
 
   def update
-    if @project.update_attributes(project_params)
-      redirect_to @project, notice: "Project was successfully updated."
-    else
-      flash.now[:error] = "Project could not be saved."
-      render :edit
+    respond_to do |format|
+      format.html do
+        if @project.update_attributes(project_params)
+          redirect_to @project, notice: "Project was successfully updated."
+        else
+          flash.now[:error] = "Project could not be saved."
+          render :edit
+        end
+      end
+      format.js do
+        if @project.update_attributes(project_params)
+        else
+          render text: @project.errors.full_messages.join(". "), status: :unprocessable_entity
+        end
+      end
     end
   end
 
   def destroy
     @project.destroy
-    redirect_to projects_path
+    respond_to do |format|
+      format.html { redirect_to projects_path }
+      format.js
+    end
   end
 
   private
